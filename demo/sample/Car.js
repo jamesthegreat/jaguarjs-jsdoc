d0318f1714cb81c9760375672e5490eedaa11455
James Ballaban <jballaban@klick.com>
2016-01-19 12:39:00 -0500

    - Added line numbers to properties - Added linenums to demo - members with a type show up as properties - alphabetical menu

--- demo/sample/Car.js
+++ demo/sample/Car.js
@@ -1,31 +1,33 @@
+"use strict";
+
 (function (w) {
     /**
      * quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-     * @class
-     * @name namespace.Car
      */
-    w.namespace.Car = function () {
-    };
-
-    w.namespace.Car.prototype = /** @lends namespace.Car.prototype */{
-        /**
-         * Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-         */
-        testMethod: function () {
-        },
-
-        testMethodUnNotated: function () {
-        },
-
-        testEvent: function () {
-            /**
-             * tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-             * @name namespace.Car#testEvent
-             * @event
-             * @param {Event} e
-             */
-            test.trigger();
+    namespace.Car = class {
+        constructor() {
+            this._colour='#000';
         }
-    };
+
+        /**
+        The current colour (HEX) state
+        @type {string}
+        **/
+        set colour(val) {
+            this._colour = val;
+        }
+        get colour() {
+            return this._colour;
+        }
+
+        /**
+        Set the current colour (HEX) state
+        @param {string} - The current HTML colour code (HEX)
+        **/
+        test(input) {
+
+        }
+    }
+
 })(window);
