c45f374f17e23f493b3fb7b0bd1dcbde46ee0c21
James Ballaban <jballaban@klick.com>
2016-01-20 10:08:42 -0500

    - Added override flag to members and methods

--- demo/sample/Car.js
+++ demo/sample/Car.js
@@ -22,11 +22,41 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         }
 
         /**
+        Sets the current car make
+        @type {string}
+        **/
+        set make(val) {
+            this._make = val;
+        }
+
+        /**
         Set the current colour (HEX) state
         @param {string} - The current HTML colour code (HEX)
         **/
         test(input) {
 
+        }
+    }
+
+    /**
+    Honda class
+    @extends namespace.Car
+    **/
+    namespace.Honda = class extends namespace.Car {
+        
+        /**
+        Overrides base make
+        @type {string}
+        **/
+        set make(val) {
+            super.make = val;
+        }
+
+        /**
+        Honda input then calls base.
+        **/
+        test (input) {
+            super.test(input);
         }
     }
 
